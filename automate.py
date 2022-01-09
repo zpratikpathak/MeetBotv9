c1e7b8a4f5218d036e38479ec5cee671ab7e7354
Pratik Pathak <pratikpathak200@gmail.com>
2022-01-09 20:55:03 +0530

    Optimized

--- automate.py
+++ automate.py
@@ -6,6 +6,7 @@ from chromium import browser
 
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -14,22 +15,24 @@ USER_ID = os.getenv("USER_ID")
 updater = Updater(token=BOT_TOKEN, use_context=True)
 dp = updater.dispatcher
 
-# autowrite userid in .env file
+# Feature addition autowrite userid in .env file
 def start(update, context):
     user = update.message.from_user
     # print(user)
-    context.bot.send_chat_action(chat_id=user['id'], action=ChatAction.TYPING)
-    update.message.reply_text('Hello {}!'.format(user['first_name']))
-    update.message.reply_text('Your UserID is: {} '.format(user['id']))
+    context.bot.send_chat_action(chat_id=user["id"], action=ChatAction.TYPING)
+    update.message.reply_text("Hello {}!".format(user["first_name"]))
+    update.message.reply_text("Your UserID is: {} ".format(user["id"]))
+
 
 def echo(update, context):
     update.message.reply_text(
         "This is not a valid command\nUse /help to list out the available commands"
     )
 
+
 def help(update, context):
     user = update.message.from_user
-    if user['id'] == int(USER_ID):
+    if user["id"] == int(USER_ID):
         context.bot.send_message(
             chat_id=USER_ID,
             text="/mlogin - To login your account\n/meet - To join a meet (Use Example: '/meet https://meet.google.com/mee-tco-deval')\n/status - To get current Screenshot of Joined meet\n/exit - To leave a meeting\n/reset - To reset chromium browser (in Development)\n/owner-To know about me\n/help - To Display this message",
@@ -39,8 +42,12 @@ def help(update, context):
             "You are not authorized to use this bot.\nUse /owner to know about me"
         )
 
+
 def owner(update, context):
-    update.message.reply_text("My code lies around the whole Internet 😇\nIt was assembled, modified and upgraded by Pathak Pratik\nSource Code is available here👇\nhttps://github.com/zpratikpathak/\n/userid - Your Userid",)
+    update.message.reply_text(
+        "My code lies around the whole Internet 😇\nIt was assembled, modified and upgraded by Pathak Pratik\nSource Code is available here👇\nhttps://github.com/zpratikpathak/\n/userid - Your Userid",
+    )
+
 
 def main():
     dp.add_handler(CommandHandler("start", start, run_async=True))
