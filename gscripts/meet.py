a70b226fdd43e25bcfbac91c7a5e4a30fc58de32
Pratik Pathak <pratikpathak200@gmail.com>
2022-01-11 09:34:09 +0530

    Added multiple meeturl type

--- gscripts/meet.py
+++ gscripts/meet.py
@@ -1,5 +1,7 @@
 from chromium_Scripts import browser
 from automate import updater
+from telegram import ChatAction
+
 
 import os
 from dotenv import load_dotenv
@@ -8,17 +10,36 @@ load_dotenv()
 USER_ID = os.getenv("USER_ID")
 
 
-def meet(update, context):
+def meet_url(context, url_meet):
+    browser.get(url_meet)
+    context.bot.send_message(chat_id=USER_ID, text="Joined the meet")
 
-    context.bot.send_chat_action(chat_id=userId, action=ChatAction.TYPING)
-    url_meet = update.message.text.split()[-1]
-    if len(url_meet) > 5:
-        joinMeet(context, url_meet)
+
+def meet(update, context):
+    user = update.message.from_user
+    if user["id"] == int(USER_ID):
+        context.bot.send_chat_action(chat_id=USER_ID, action=ChatAction.TYPING)
+        url_meet = update.message.text.split()[-1]
+        if len(url_meet) == 12:
+            url_meet = "https://meet.google.com/{}".format(url_meet)
+            meet_url(context, url_meet)
+        elif len(url_meet) == 10:
+            url_meet = url_meet[:3] + "-" + url_meet[3:5] + "-" + url_meet[5:]
+            url_meet = "https://meet.google.com/{}".format(url_meet)
+            meet_url(context, url_meet)
+        elif len(url_meet) > 5:
+            meet_url(context, url_meet)
+        else:
+            context.bot.send_message(
+                chat_id=USER_ID, text="Oops! You forget to pass the google meet url"
+            )
+            context.bot.send_message(
+                chat_id=USER_ID, text="Use /meet command like this 👇"
+            )
+            context.bot.send_message(
+                chat_id=USER_ID, text="/meet https://meet.google.com/meet-code-value"
+            )
     else:
-        context.bot.send_message(
-            chat_id=userId, text="Oops! You forget to pass the google meet url"
-        )
-        context.bot.send_message(chat_id=userId, text="Use /meet command like this 👇")
-        context.bot.send_message(
-            chat_id=userId, text="/meet https://meet.google.com/meet-code-value"
+        update.message.reply_text(
+            "You are not authorized to use this bot.\nUse /owner to know about me"
         )
